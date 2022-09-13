7a6d1da5cc2d7e72259c892761fde5407cbf1394
Aleksandr Medvedev <the.dreams.wind@gmail.com>
2022-09-13 14:10:20 +0300

    Initial Commit

---
+++ SerialDataTaskSequence/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SerialDataTaskSequence
+//
+//  Created by Aleksandr Medvedev on 13.09.2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SerialDataTaskSequence/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SerialDataTaskSequence
+//
+//  Created by Aleksandr Medvedev on 13.09.2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
